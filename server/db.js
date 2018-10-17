1864af9e91b6bb621204c829a94d1f86921cc9c2
Daniel Magley <danielmagley@Daniels-MacBook-Pro.local>
2018-10-17 17:10:17 -0400

    jjjsjs

--- server/db.js
+++ server/db.js
@@ -1,8 +1,14 @@
 const Sequelize = require("sequelize");
 
-const sequelize = new Sequelize(process.env.DATABASE_URL, {
-  dialect: "postgres"
-});
+const sequelize = new Sequelize(
+  process.env.DATABASE_URL ||
+    `postgresql://postgres:${encodeURIComponent(
+      process.env.PASS
+    )}@localhost/serverproject`,
+  {
+    dialect: "postgres"
+  }
+);
 sequelize.authenticate().then(
   () => {
     console.log("Connected to project database");
